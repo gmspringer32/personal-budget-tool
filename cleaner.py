a9df316f7a28a0550abc6dec57faa984241d2b18
Garrett Springer <gmspringer32@gmail.com>
2023-11-21 11:27:09 -0700

    initial commit

---
+++ cleaner.py
@@ -0,0 +1,106 @@
+import pandas as pd
+
+from categories import store_categories
+
+class Cleaner:
+    def __init__(self, checking_df, credit_card_df):
+        self.checking_df = checking_df
+        self.credit_card_df = credit_card_df
+        self.accounts_df = pd.DataFrame()
+
+
+    def getAccountsDf(self):
+        return self.accounts_df
+    
+
+
+    def clean(self):
+        self.__cleanCreditCardTransactions__()
+        self.__cleanBankTransactions__()
+        self.__addCategoryColumn__()
+        self.__combineDfs__()
+        self.__sortDates__()
+        self.accounts_df.reset_index(drop=True, inplace=True)
+
+
+
+
+    def __cleanCreditCardTransactions__(self):
+        self.__dropUnusedColumns__()
+        self.__renameColumns__()
+        self.__dropUnusedRows__()
+        self.__cleanDescription__()
+        self.__convertDateToDateObject__()
+        self.__convertMoneyToNumber__()
+        self.credit_card_df['account'] = 'Credit Card'
+        
+        self.credit_card_df.reset_index(inplace=True, drop = True)
+
+        
+    def __dropUnusedColumns__(self):
+        self.credit_card_df.drop(self.credit_card_df.columns[2], axis=1, inplace= True)
+
+    def __renameColumns__(self):
+        self.credit_card_df.rename(columns={'sort by Posting Date  in chronological order  ↓': 'date', 
+                                        'sort by Description in alphabetical order': 'description', 
+                                        'sort by Amount in ascending order':'amount', 
+                                        'Balance':'balance'}, inplace=True)
+    def __dropUnusedRows__(self):
+        self.credit_card_df = self.credit_card_df.copy()
+        self.credit_card_df.drop(self.credit_card_df.loc[self.credit_card_df['date'] == 'There are no transactions to display for the filter you selected. Try changing your selection in the Type filter.'].index, inplace=True)
+        self.credit_card_df.drop(self.credit_card_df.loc[self.credit_card_df['date'].str.contains('Beginning balance as of')].index, inplace=True)
+        self.credit_card_df.drop(self.credit_card_df.loc[self.credit_card_df['date'].str.contains('link not applicable for screen reader users')].index, inplace=True)
+        self.credit_card_df.drop(self.credit_card_df.loc[self.credit_card_df['date'] == "Pending"].index, inplace=True)
+    
+    def __cleanDescription__(self):
+        self.credit_card_df = self.credit_card_df.copy()
+        self.credit_card_df['description'] = self.credit_card_df['description'].str.split('transaction').str.get(1).str.strip('for Transaction date: ').str.split('Add this deal').str.get(0).str.replace(r'(\d{2}\/\d{2}\/\d{4}.)', '', regex=True)
+
+    def __convertDateToDateObject__(self):
+        self.credit_card_df = self.credit_card_df.copy()
+        self.credit_card_df['date'] = pd.to_datetime(self.credit_card_df['date'])
+
+    def __convertMoneyToNumber__(self):
+        self.credit_card_df = self.credit_card_df.copy()
+        self.credit_card_df['amount'] = pd.to_numeric(self.credit_card_df['amount'].str.replace('$', "",regex=False).str.replace(",", '', regex=False))
+        self.credit_card_df['balance'] = pd.to_numeric(self.credit_card_df['balance'].str.replace('$', "",regex=False).str.replace(",",'',regex=False))
+
+
+
+
+
+    def __cleanBankTransactions__(self):
+        self.checking_df = self.checking_df.copy()
+        self.checking_df.rename(columns={'Posting date' : 'date', 'Description':'description', 'Amount':'amount', 'Available balance':'balance', 'Type':'category'}, inplace=True)
+        self.checking_df.drop(columns=['Reconcile', 'Unnamed: 6'], inplace=True)
+        desired_order = ['date', 'description', 'amount', 'balance', 'category']
+        self.checking_df = self.checking_df[desired_order]
+        self.checking_df.drop(self.checking_df.loc[self.checking_df['date'].str.contains('Statement')].index, inplace=True)
+        self.checking_df['description'] = self.checking_df['description'].str.replace('View/Edit', '')
+        self.checking_df['amount'] = pd.to_numeric(self.checking_df['amount'].str.replace('$', "",regex=False).str.replace(",", '',regex=False))
+        self.checking_df['balance'] = pd.to_numeric(self.checking_df['balance'].str.replace('$', "",regex=False).str.replace(",",'',regex=False))
+        self.checking_df.drop(self.checking_df.loc[self.checking_df['date'] == 'Processing'].index, inplace=True)
+        self.checking_df['date'] = pd.to_datetime(self.checking_df['date'])
+        self.checking_df['account'] = 'Checking'
+
+
+
+
+    def __addCategoryColumn__(self):
+        def __mapStoreToCat__(vendor_name):
+            vendor_name = vendor_name.replace("-", " ")
+            for store_name, category in store_categories.items():
+                if store_name in vendor_name:
+                    return category
+                
+            return 'Other'
+        self.credit_card_df['category'] = self.credit_card_df['description'].apply(lambda x: __mapStoreToCat__(x))
+
+
+
+
+    def __combineDfs__(self):
+        self.accounts_df = pd.concat([self.credit_card_df, self.checking_df])   
+
+    def __sortDates__(self):
+        self.accounts_df.sort_values(by='date', ascending=False, inplace=True)
---
+++ scraper.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+import datetime
+from selenium.webdriver.chrome.options import Options
+
+from cleaner import Cleaner
+
+class BankScraper:
+    def __init__(self):
+        with open("bankurl.txt") as file:
+            self.url = file.read()
+        self.driver = None
+        self.credit_card_df = pd.DataFrame()
+        self.checking_df = pd.DataFrame()
+        self.accounts_df = pd.DataFrame()
+        with open("creditcardaccount") as file:
+            self.credit_card_account = file.read()
+        with open("bankaccount") as file:
+            self.bank_account = file.read()
+        
+
+    def __startDriver__(self):
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.driver.get(self.url)
+
+    def __insertUsername__(self, username):
+        username_bar = self.driver.find_element(By.ID, 'onlineId1')
+        time.sleep(.5)
+        username_bar.click()
+        username_bar.send_keys(username)
+        time.sleep(.5)
+
+    def __insertPassword__(self, password):
+        password_bar = self.driver.find_element(By.ID, "passcode1")
+        time.sleep(.5)
+        password_bar.click()
+        password_bar.send_keys(password)
+        time.sleep(.5)
+
+    def __clickLogin__(self):
+        signin_bar = self.driver.find_element(By.ID, 'signIn')
+        signin_bar.click()
+
+    def login(self, username, password):
+        self.__startDriver__()
+        self.__insertUsername__(username)
+        self.__insertPassword__(password)
+        self.__clickLogin__()
+
+    def __openAccount__(self, account_string):
+        accounts = self.driver.find_elements(By.CLASS_NAME, "AccountName")
+        wanted_account = account_string
+        account_element = None
+        for account in accounts:
+            if account.text == wanted_account:
+                account_element = account
+        account_element.click()        
+
+    def __scrapeCreditCardTransactions__(self):
+        date_input = self.driver.find_element(By.CLASS_NAME, 'select-bofa')
+        date_input_elements = date_input.find_elements(By.TAG_NAME, 'option')
+        min_num = min(12, len(date_input_elements))
+        for i in range(0, min_num):
+            date_input = self.driver.find_element(By.CLASS_NAME, 'select-bofa')
+            date_input.find_elements(By.TAG_NAME, 'option')[i].click()
+            transactions = self.driver.find_element(By.ID, "transactions")
+            table_html = transactions.get_attribute('outerHTML')
+            df = pd.read_html(table_html)[0]
+            self.credit_card_df = pd.concat([self.credit_card_df, df])
+
+    
+    def __closeAccount__(self):
+        self.driver.find_element(By.LINK_TEXT, 'Accounts').click()
+
+    def __scrapeBankTransactions__(self):
+        time.sleep(1.5)
+        self.checking_df = pd.read_html(self.driver.find_elements(By.TAG_NAME, 'table')[1].get_attribute('outerHTML'))[0]
+
+    
+    
+
+    def getAccountTransactions(self, month = None):
+        month_nums = range(1,12) 
+        if month not in month_nums and month is not None:
+            print("Please input month as a number, Jan = 1")
+            return
+    
+        self.__openAccount__(self.credit_card_account)
+        self.__scrapeCreditCardTransactions__()
+        self.__closeAccount__()
+        self.__openAccount__(self.bank_account)
+        self.__scrapeBankTransactions__()
+        cleaner = Cleaner(self.checking_df, self.credit_card_df)
+        cleaner.clean()
+        return cleaner.getAccountsDf()
+    
+    def logout(self):
+        self.driver.quit()
